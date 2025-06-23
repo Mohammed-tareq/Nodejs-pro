8383457e632ba44b9e238886317dc79343f2ec91
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-06-23 20:52:30 +0300

    Refactor error handling middleware to include specific handling for duplicate MongoDB entries

--- Middlewares/errorHandel.js
+++ Middlewares/errorHandel.js
@@ -1,15 +1,21 @@
 import AppErorr from "../Utils/AppErorr.js";
+import mongoose from "mongoose";
 
-export default(err, req,res,next) =>{
+export default (err, req, res, next) => {
 
     console.log(err.stack);
-    if(err instanceof AppErorr) {
+    if (err instanceof AppErorr) {
         return res.status(err.status).json({
-            status:"Failed",
-            message : err.message
+            status: "Failed",
+            message: err.message
         })
     }
 
 
-    if(err.code)
+    if (err.code === "11000" || err instanceof mongoose.mongo.MongoServerError && err.code === 11000  ) {
+        return res.status(400).json({
+            status: "Failed",
+            message: `Duplicate entry ${Object.keys(err.keyValue)[0]}`
+        })
+    }
 }
