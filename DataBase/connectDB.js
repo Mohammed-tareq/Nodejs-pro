9c7445a94d05d3bcf06e9b21a2b2cde4fa1e6ed6
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-06-23 18:50:05 +0300

    Refactor database connection in connectDB.js to include connection name in success log and exit process on failure

--- DataBase/connectDB.js
+++ DataBase/connectDB.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+    import mongoose, { connections } from "mongoose";
+    import dotenv from "dotenv";
+    dotenv.config();
 
-
-export default async function connectDB (){
-
+    export default async function connectDB() {
     mongoose.connect(process.env.DB_URL)
-    .then(() => console.log("connected to DB"))
-    .catch(() => console.log ("failed to connect to DB"))
-}
+        .then((conn) => console.log(`connected to DB ${conn.connection.name}`))
+        .catch(() => {
+        console.log("failed to connect to DB");
+        process.exit(1); // this will exit the process  and make  stop run node app
+        });
+    }
