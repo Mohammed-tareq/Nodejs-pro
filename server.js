7c4ea59e2e27ec1a7a2b2c32bf01ab2c8af1fb34
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-06-23 17:31:38 +0300

    Initialize dotenv configuration in server.js and set up port from environment variables.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import env from "dotenv";
 
+env.config();
 const app = express();
-
+const port  = process.env.PORT;
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
