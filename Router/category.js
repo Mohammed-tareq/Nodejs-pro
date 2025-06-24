35651173aae83f07f174c87d52547bcb9c314dbe
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-06-24 10:15:46 +0300

    Refactor subcategory routes and controllers for improved functionality and logging

--- Router/category.js
+++ Router/category.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import categoryIndex from "../Controllers/CategoryController/categoryIndex.js";
 import {getCategoryValidator,createCategoryValidator,updateCategoryValidator,deleteCategoryValidator} from "../Utils/categoryValidator.js";
+import subCategoryRouter from "./subCategory.js";
+
 const categoryRouter = Router();
 
-// categoryRouter.post("/" , categoryIndex.addCategory);
-// categoryRouter.get("/" , categoryIndex.getCategories);
-// or DRY
-categoryRouter.route("/").get(categoryIndex.getCategories).post(createCategoryValidator,categoryIndex.addCategory);
 
+
+categoryRouter.route("/").get(categoryIndex.getCategories).post(createCategoryValidator,categoryIndex.addCategory);
 categoryRouter.route("/:id").get(getCategoryValidator,categoryIndex.getCategory).put(updateCategoryValidator,categoryIndex.updateCategory).delete(deleteCategoryValidator,categoryIndex.deleteCategory);
 
+categoryRouter.use("/:categoryId/subCategory", subCategoryRouter);
+categoryRouter.use((req, res, next) => {
+  console.log("categoryRouter hit:", req.method, req.originalUrl, req.params);
+  next();
+});
 
 export default categoryRouter;
